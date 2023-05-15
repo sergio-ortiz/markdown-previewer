ac7bb5526493bdbd40cb331df962f7fada4f4bcb
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-15 14:02:49 -0400

    feat(bootstrap): textarea .form-control customization

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ function App() {
   return (
     <div className="container-fluid bg-primary vh-100">
       <div className="row vh-100 justify-content-center">
-        <textarea id="editor" className="col-5 m-5 bg-light rounded">
-        </textarea>
+        <div className="col-5 m-5 bg-light rounded">
+          <textarea id="editor" className="form-control form-control-lg shadow-none bg-light border border-0" autoFocus={true} />
+        </div>
         <div id="preview" className="col-5 m-5 bg-light rounded">
         </div>
       </div>
