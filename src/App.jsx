d808f10153da3780b61f4a81e416691eda067892
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-16 20:26:34 -0400

    feat(marked): for rendering markdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useDispatch, useSelector } from "react-redux"
 import { userInput } from "./features/editor/editorSlice";
+import { marked } from "marked";
+import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
   const editor = useSelector(state => state.editor.value);
+
+  
+  useEffect(() => {
+    dispatch(userInput("# Main Heading\n## subheading\n__bold text__\n```\nfunction() {\nreturn 'Hello World'\n}\n```\nas opposed to the modern syntax, `() => 'Hello World'`.\n> This a quote\n\n![vite logo](/vite.svg)\n\n[Click here](https://www.devserg.io)\n\n- list item"))
+  }, [])
 
   return (
     <div className="container-fluid bg-primary vh-100">
@@ -14,12 +21,15 @@ function App() {
             className="form-control form-control-lg shadow-none bg-light border border-0 h-100"
             autoFocus={true} 
             spellCheck={false}
+            defaultValue={editor}
             onChange={(event) => dispatch(userInput(event.target.value))}
           />
         </div>
-        <div id="preview" className="col-5 m-5 bg-light rounded fs-5 p-2 ps-4">
-          {editor}
-        </div>
+        <div 
+          id="preview" 
+          className="col-5 m-5 bg-light rounded fs-5 p-2 ps-4" 
+          dangerouslySetInnerHTML={{ __html: marked(editor, {mangle: false, headerIds: false, gfm: true, breaks: true}) }} 
+        />
       </div>
     </div>
   )
